cee52a4ac241cb2b25c0478e95598be85de7d5d9
AnimeBoyYt <abhishekaglawe00@gmail.com>
2022-05-26 21:33:21 +0530

    final commit

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 function msg() {
-
-        var br = "%0A";
-        var phone = document.getElementById("phone").value.replace(/\s/g, "");
-        var msgcontent = document.getElementById("msgbox").value;
-        var regex = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/;
+        const phone = document.getElementById("phone").value.replace(/\s/g, "");
+        const msgcontent = document.getElementById("msgbox").value;
+        const regex = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/;
 
         if (phone.length == 0) {
                 alert("Mobile number cannot be empty!");
         }
         else if (regex.test(phone)) {
-                var link = "https://api.whatsapp.com/send/?phone=91" + phone.slice(-10) + "&text=" + msgcontent;
+                const link = "https://api.whatsapp.com/send/?phone=91" + phone.slice(-10) + "&text=" + msgcontent;
                 window.open(link, "_blank");
         }
         else {
                 alert("Invalid Mobile Number!");
         }
+}
+document.querySelector('#form').addEventListener('submit', msg);
 
+
+const countEl = document.getElementById("count");
+
+countvisits();
+function countvisits() {
+  fetch("https://api.countapi.xyz/update/newchats.in/newchats.in/?amount=1")
+  .then((res) => res.json())
+  .then((res) => {
+    countEl.innerHTML = res.value;
+  });
 }
